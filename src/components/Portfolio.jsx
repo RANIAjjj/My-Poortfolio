aa101ae17361bc544baa13c9dd2a43fd87e1f488
RANIAjjj <ranyamohamed381@gmail.com>
2024-10-16 17:10:27 +0200

    edited project section

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -28,10 +28,10 @@ function Portfolio() {
     <>
       <nav className="shadow-lg">
         <div className="nav-container">
-          <div className="brand text-2xl font-bold">
+          {/* <div className="brand text-2xl font-bold">
             <h2 className="brand-icon">R</h2>
             <h3 className="brand-name">Rania</h3>
-          </div>
+          </div> */}
           <ul className="nav-links text-lg font-semibold">
             <li>
               <a onClick={() => scrollToSection("header")}>Home</a>
@@ -46,7 +46,10 @@ function Portfolio() {
               <a onClick={() => scrollToSection("projects")}>Projects</a>
             </li>
             <li>
-              <a>Services</a>
+              <a>Skills</a>
+            </li>
+            <li>
+              <a>Achievements</a>
             </li>
             <button>Contact</button>
           </ul>
@@ -63,12 +66,13 @@ function Portfolio() {
         </div>
       </nav>
       <header id="header">
+        <div className="absolute left-0 top-0 -z-10 h-[100dvh] w-full bg-gradient-to-r from-pink-300 via-purple-300 to-indigo-300 opacity-30 blur-3xl"></div>
         <div className="header-container">
           <div className="header-text">
             <p className="header-text-title text-4xl font-semibold">
               Hello , I'm Rania Mohamed
             </p>
-            <p className="header-text-description text-slate-500 text-base">
+            <p className="header-text-description text-slate-500 text-lg">
               I'm a Freelance Frontend Developer based in Aswan,Egypt . I
               strives to build immersive and beautiful web applications through
               carefully crafted code and user-centric design.
@@ -129,10 +133,10 @@ function Portfolio() {
             </div>
           </div>
           <div className="about-part2">
-            <p className="header-text-title text-3xl font-semibold">
-              I am Professional Frontend Developer
+            <p className="header-text-title text-4xl font-semibold">
+              I am a Professional Frontend Developer
             </p>
-            <p className="header-text-description text-slate-500 text-base">
+            <p className="header-text-description text-slate-500 text-lg">
               I am skilled in React with expertise in JavaScript, TypeScript,
               React Context and Redux Toolkit. has experience in building
               dynamic web applications, handling APIs. Proficient in front-end
@@ -151,8 +155,8 @@ function Portfolio() {
       <section id="process" className="process-section">
         <div className="process-container">
           <div className="process-title">
-            <p className="text-2xl font-semibold">Work Process</p>
-            <p className="text-slate-500 text-base">
+            <p className="text-3xl font-semibold">Work Process</p>
+            <p className="text-slate-500 text-xl">
               My work process include series of steps that are systematically
               followed to complete a task or achieve a specific goal in a
               project . It involves defining the workflow, actions, and stages
@@ -166,8 +170,8 @@ function Portfolio() {
                 <p className="item-icon ">
                   <CalendarMonthOutlinedIcon sx={{ color: purple["A700"] }} />
                 </p>
-                <p className="text-sm font-semibold">1. Planning</p>
-                <p className="text-slate-500 text-sm">
+                <p className="text-md font-semibold">1. Planning</p>
+                <p className="text-slate-500 text-md">
                   Understand the project scope, user needs, and technical
                   requirements.
                 </p>
@@ -176,8 +180,8 @@ function Portfolio() {
                 <p className="item-icon">
                   <CodeOutlinedIcon sx={{ color: purple["A700"] }} />
                 </p>
-                <p className="text-sm font-semibold">2. Coding </p>
-                <p className="text-slate-500 text-sm">
+                <p className="text-md font-semibold">2. Coding </p>
+                <p className="text-slate-500 text-md">
                   Set up the development environment. Build the user interface.
                   Develop reusable components and integrate APIs for dynamic
                   functionality.
@@ -189,8 +193,8 @@ function Portfolio() {
                 <p className="item-icon">
                   <AutoFixHighOutlinedIcon sx={{ color: purple["A700"] }} />
                 </p>
-                <p className="text-sm font-semibold">3. Optimization </p>
-                <p className="text-slate-500 text-sm">
+                <p className="text-md font-semibold">3. Optimization </p>
+                <p className="text-slate-500 text-md">
                   Enhance performance. Ensure responsiveness across devices
                 </p>
               </div>
@@ -198,8 +202,8 @@ function Portfolio() {
                 <p className="item-icon">
                   <DevicesOutlinedIcon sx={{ color: purple["A700"] }} />
                 </p>
-                <p className="text-sm font-semibold">4. Deployment </p>
-                <p className="text-slate-500 text-sm">
+                <p className="text-md font-semibold">4. Deployment </p>
+                <p className="text-slate-500 text-md">
                   Prepare the application for production. Deploy the front-end
                   code to hosting platforms (Netlify, Vercel).
                 </p>
@@ -210,17 +214,19 @@ function Portfolio() {
       </section>
       <section id="projects" className="projects-section">
         <div className="portfolio-container">
-          <p className="text-3xl font-semibold">Projects</p>
+          <p className="text-4xl font-semibold">Projects</p>
+          <p className="text-xl opacity-75">A collection of my recent work</p>
           <div className="portfolio-projects">
             <div className="project hover:shadow-2xl hover:scale-105">
               <img
                 src={labtop}
-                width={300}
-                style={{ borderRadius: "7px 7px 0 0" }}
+                width={490}
+                style={{ borderRadius: "10px" }}
               />
               <div className="project-content">
-                <p className="font-bold text-sm">Landing Page</p>
-                <p className="text-slate-500 text-xs">Web design</p>
+                <p className="font-semibold text-xl">Landing Page</p>
+                <p className="text-slate-500 text-lg">Web design</p>
+                <p className="text-slate-500 text-lg">Technologies used: React</p>
                 <div className="buttons">
                   <button className="text-sm font-semibold">
                     Live <RemoveRedEyeIcon />{" "}
@@ -234,12 +240,13 @@ function Portfolio() {
             <div className="project hover:shadow-2xl hover:scale-105">
               <img
                 src={labtop}
-                width={300}
-                style={{ borderRadius: "7px 7px 0 0" }}
+                width={490}
+                style={{ borderRadius: "10px" }}
               />
               <div className="project-content">
-                <p className="font-bold text-sm">Landing Page</p>
-                <p className="text-slate-500 text-xs">Web app</p>
+                <p className="font-bold text-xl">Landing Page</p>
+                <p className="text-slate-500 text-lg">Web app</p>
+                <p className="text-slate-500 text-lg">Technologies used: React</p>
                 <div className="buttons">
                   <button className="text-sm font-semibold">
                     Live <RemoveRedEyeIcon />{" "}
@@ -253,12 +260,13 @@ function Portfolio() {
             <div className="project hover:shadow-2xl hover:scale-105">
               <img
                 src={labtop}
-                width={300}
-                style={{ borderRadius: "7px 7px 0 0" }}
+                width={490}
+                style={{ borderRadius: "10px" }}
               />
               <div className="project-content">
-                <p className="font-bold text-sm">Landing Page</p>
-                <p className="text-slate-500 text-xs">Web design</p>
+                <p className="font-bold text-xl">Landing Page</p>
+                <p className="text-slate-500 text-lg">Web design</p>
+                <p className="text-slate-500 text-lg">Technologies used: React</p>
                 <div className="buttons">
                   <button className="text-sm font-semibold">
                     Live <RemoveRedEyeIcon />{" "}
@@ -272,12 +280,13 @@ function Portfolio() {
             <div className="project hover:shadow-2xl hover:scale-105">
               <img
                 src={labtop}
-                width={300}
-                style={{ borderRadius: "7px 7px 0 0" }}
+                width={490}
+                style={{ borderRadius: "10px" }}
               />
               <div className="project-content">
-                <p className="font-bold text-sm">Landing Page</p>
-                <p className="text-slate-500 text-xs">Web app</p>
+                <p className="font-bold text-xl">Landing Page</p>
+                <p className="text-slate-500 text-lg">Web app</p>
+                <p className="text-slate-500 text-lg">Technologies used: React</p>
                 <div className="buttons">
                   <button className="text-sm font-semibold">
                     Live <RemoveRedEyeIcon />{" "}
@@ -299,7 +308,7 @@ function Portfolio() {
           <p className="text-3xl font-semibold text-slate-50">
             Let's discuss your project !
           </p>
-          <button className="text-sm font-semibold text-slate-50">
+          <button className="text-md font-semibold text-slate-50">
             Let’s work Together <EastIcon fontSize="small" />
           </button>
         </div>
